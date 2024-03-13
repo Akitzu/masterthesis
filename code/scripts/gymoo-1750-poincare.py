5151a6b007651610c5ba68fe7a3e1662369dcac3
Akitzu <ludovic.rais@epfl.ch>
2024-03-13 10:20:26 +0100

    GYM poincare setting up for jed

--- code/scripts/gymoo-1750-poincare.py
+++ code/scripts/gymoo-1750-poincare.py
@@ -1,6 +1,6 @@
 from simsopt.configs import get_w7x_data
 from simsopt.field import Current
-from mpi4py import MPI
+from simsopt.util import comm_world
 import numpy as np
 import datetime
 import pickle
@@ -9,68 +9,51 @@ import argparse
 # Adding the path to the horus package
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
+ROOT_DIR = os.path.join(os.path.dirname(__file__), '..')
+sys.path.append(os.path.abspath(ROOT_DIR))
 import horus as ho
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Convergence domain for GYM000-1750")
+    args = argparse.ArgumentParser(description="Poincare for GYM000-1750")
+    args.add_argument("-phis", nargs='+', type=float, default=[0], help="Phi coordinates for the Poincare plot")
+    args.add_argument("-tol", type=float, default=1e-10, help="Tolerance for the Poincare plot")
+    # parser.add_argument("-r", nargs='+', type=float, help="R coordinates for the Poincare plot")
+    # parser.add_argument("-z", nargs='+', type=float, help="Z coordinates for the Poincare plot") 
 
-    # R = np.linspace(5.4, 6.26, 44)
-    # Z = np.linspace(-1, 1, 101)
-    R = np.linspace(5.4, 6.26, 3)
-    Z = np.linspace(-1, 1, 3)
+    # Setup the lines initial coordinates    
+    nfieldlines = 30
+    Rs = np.linspace(6.05, 6.2, nfieldlines)
+    Zs = [ma.gamma()[0, 2] for _ in range(nfieldlines)]
+    RZs = np.array([[r, z] for r, z in zip(Rs, Zs)])
 
-    options = {
-        "pp": 3,
-        "qq": 7,
-        "sbegin": 5,
-        "send": 6.5,
-        "tol": 1e-8,
-        "rtol": 1e-10,
-        "checkonly": True,
-        "eps": 1e-5,
-    }
+    nfieldlines = 5
+    p1 = np.array([5.6144507858315915, -0.8067790944375764])
+    p2 = np.array([5.78, -0.6])
+    Rs = np.linspace(p1[0], p2[0], nfieldlines)
+    Zs = np.linspace(p1[1], p2[1], nfieldlines)
+    Rs, Zs = np.meshgrid(Rs, Zs)
+    RZs2 = np.array([[r, z] for r, z in zip(Rs.flatten(), Zs.flatten())])
 
-    # Get the W7-X data for the GYM000-1750 configuration
-    w7x = get_w7x_data()
+    RZs = np.concatenate((RZs, RZs2))
 
+    # Current configuration
     currents = [Current(1.109484) * 1e6 for _ in range(5)]
     currents.append(Current(-0.3661) * 1e6)
     currents.append(Current(-0.3661) * 1e6)
 
+    # Get the W7-X data for the GYM000-1750 configuration
+    w7x = get_w7x_data()
+
     bs, bsh, (nfp, coils, ma, sc_fieldline) = ho.stellarator(w7x[0], currents, w7x[2], nfp=5, surface_radius=2)
 
-    # Compute the convergence domain
-    ps = ho.SimsoptBfieldProblem(ma.gamma()[0, 0], 0, 5, bs)
+    _, _, fig, ax = ho.poincare(bsh, RZs, args.phis, sc_fieldline, tol = args.tol, comm = comm_world)
 
-    # Initialize MPI
-    comm = MPI.COMM_WORLD
-    rank = comm.Get_rank()  # Rank of the current process
-    size = comm.Get_size()  # Total number of processes
-    
-    # Divide the work among the processes
-    R_split = np.array_split(R, size)[rank]
-    Z_split = np.array_split(Z, size)[rank]
+    if comm_world is None or comm_world.rank == 0:
+        for col in ax[0,0].collections:
+            col.set_color('black')
+            col.set_sizes([0.5])
 
-    # Perform the calculation for this process's subset of R and Z
-    convdom_checkonly_local = ho.convergence_domain(ps, R_split, Z_split, **options)
-
-    # Gather the results to the root process
-    convdom_checkonly = comm.gather(convdom_checkonly_local, root=0)
-
-    # The root process saves the result
-    if rank == 0:
-        # Concatenate the list of results
-
-        convdom_checkonly_joined = convdom_checkonly[0]
-        convdom_checkonly = convdom_checkonly[1:]
-        for convdom in convdom_checkonly:
-            convdom_checkonly_joined = ho.join_convergence_domains(convdom_checkonly_joined, convdom)
-
-        # Save the result
         date = datetime.datetime.now().strftime("%Y%m%d")
-        dumpname = f"convergence_domain_GYM000-1750_{date}.pkl"
-        with open(os.path.join('..', 'output', dumpname), 'wb') as f:
-            pickle.dump(convdom_checkonly_joined, f)
+        dumpname = f"poincare_GYM000-1750_{date}.pkl"
+        pickle.dump(fig, open(os.path.join(ROOT_DIR, "/output/", dumpname), "wb"))
