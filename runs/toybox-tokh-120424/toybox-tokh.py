ac51486e2a538cd288b8c2f5158cf022bf999d83
Akitzu <ludovic.rais@epfl.ch>
2024-04-15 00:39:21 +0200

    save before the night

--- runs/toybox-tokh-120424/toybox-tokh.py
+++ runs/toybox-tokh-120424/toybox-tokh.py
@@ -13,18 +13,19 @@ if __name__ == "__main__":
     # gaussian10 = {"m": 1, "n": 0, "d": 1, "type": "gaussian", "amplitude": 0.01}
 
     # Creating the pyoculus problem object, adding the perturbation here use the R, Z provided as center point
-    pyoproblem = AnalyticCylindricalBfield.without_axis(
-        3,
-        0,
-        0.91,
-        0.7,
-        perturbations_args=[separatrix],
-        Rbegin=1,
-        Rend=5,
-        niter=800,
-        guess=[3.0, -0.1],
-        tol=1e-9,
-    )
+    # pyoproblem = AnalyticCylindricalBfield.without_axis(
+    #     3,
+    #     0,
+    #     0.91,
+    #     0.7,
+    #     perturbations_args=[separatrix],
+    #     Rbegin=1,
+    #     Rend=5,
+    #     niter=800,
+    #     guess=[3.0, -0.1],
+    #     tol=1e-9,
+    # )
+    pyoproblem = AnalyticCylindricalBfield(3, 0, 0.9, 0.7, perturbations_args = [separatrix])
 
     # # Adding perturbation after the object is created uses the found axis as center point
     # pyoproblem.add_perturbation(maxwellboltzmann)
@@ -44,7 +45,7 @@ if __name__ == "__main__":
     fp = FixedPoint(pyoproblem, pparams, integrator_params=iparams)
 
     # find the X-point
-    guess = [3.10, -1.656]
+    guess = [4.10, 0.]
     print(f"Initial guess: {guess}")
 
     fp.compute(guess=guess, pp=0, qq=1, sbegin=0.1, send=6, tol=1e-10)
@@ -65,46 +66,53 @@ if __name__ == "__main__":
     pparams["zeta"] = 0
 
     # # Set RZs for the normal (R-only) computation
-    # pparams["Rbegin"] = 3.01
-    # pparams["Rend"] = 5.5
+    pparams["Rbegin"] = 3.01
+    pparams["Rend"] = 5.5
 
     # Set RZs for the tweaked (R-Z) computation
-    nfieldlines = pparams["nPtrj"] + 1
-    Rs = np.linspace(3.2, 3.15, nfieldlines)
-    Zs = np.linspace(-0.43, -2.5, nfieldlines)
-    RZs = np.array([[r, z] for r, z in zip(Rs, Zs)])
+    # nfieldlines = pparams["nPtrj"] + 1
 
-    # Set RZs
-    nfieldlines = pparams["nPtrj"] + 1
-    n1, n2 = int(np.ceil(nfieldlines / 2)), int(np.floor(nfieldlines / 2))
+    # Directly setting the RZs
+    # Rs = np.linspace(3.2, 3.15, nfieldlines)
+    # Zs = np.linspace(-0.43, -2.5, nfieldlines)
+    # RZs = np.array([[r, z] for r, z in zip(Rs, Zs)])
+
+    # Two interval computation opoint to xpoint then xpoint to coilpoint
+    # n1, n2 = int(np.ceil(nfieldlines / 2)), int(np.floor(nfieldlines / 2))
+    # xpoint = np.array([results[0][0], results[0][2]])
+    # opoint = np.array([pyoproblem._R0, pyoproblem._Z0])
+    # coilpoint = np.array(
+    #     [pyoproblem.perturbations_args[0]["R"], pyoproblem.perturbations_args[0]["Z"]]
+    # )
+
+    # Simple way from opoint to xpoint then to coilpoint
+    # Rs = np.concatenate((np.linspace(opoint[0]+1e4, xpoint[0], n1), np.linspace(xpoint[0], coilpoint[0]-1e-4, n2)))
+    # Zs = np.concatenate((np.linspace(opoint[1]+1e4, xpoint[1], n1), np.linspace(xpoint[1], coilpoint[1]-1e-4, n2)))
+    # RZs = np.array([[r, z] for r, z in zip(Rs, Zs)])
+
     # Sophisticated way more around the xpoint
-    xpoint = np.array([results[0][0], results[0][2]])
-    opoint = np.array([pyoproblem._R0, pyoproblem._Z0])
-    coilpoint = np.array(
-        [pyoproblem.perturbations_args[0]["R"], pyoproblem.perturbations_args[0]["Z"]]
-    )
-
-    deps = 0.05
-    RZ1 = xpoint + deps * (1 - np.linspace(0, 1, n1)).reshape((n1, 1)) @ (
-        opoint - xpoint
-    ).reshape((1, 2))
-    RZ2 = xpoint + deps * np.linspace(0, 1, n2).reshape((n2, 1)) @ (
-        coilpoint - xpoint
-    ).reshape((1, 2))
-    RZs = np.concatenate((RZ1, RZ2))
+    # deps = 0.05
+    # RZ1 = xpoint + deps * (1 - np.linspace(0, 1, n1)).reshape((n1, 1)) @ (
+    #     opoint - xpoint
+    # ).reshape((1, 2))
+    # RZ2 = xpoint + deps * np.linspace(0, 1, n2).reshape((n2, 1)) @ (
+    #     coilpoint - xpoint
+    # ).reshape((1, 2))
+    # RZs = np.concatenate((RZ1, RZ2))
 
     # Set up the Poincare plot object
     pplot = PoincarePlot(pyoproblem, pparams, integrator_params=iparams)
 
     # # R-only computation
-    # pplot.compute()
+    pplot.compute()
 
     # R-Z computation
-    pplot.compute(RZs)
+    # pplot.compute(RZs)
 
     ### Plotting the results
+    print(f"Fixed point results: {results}")
 
-    fig, ax = pplot.plot(marker=".", s=1, xlim=[2.3, 4], ylim=[-2.9, 0.8])
+    fig, ax = pplot.plot(marker=".", s=1, xlim=[1, 6], ylim=[-2, 2])
     ax.scatter(
         pyoproblem._R0, pyoproblem._Z0, marker="o", edgecolors="black", linewidths=1
     )
@@ -112,4 +120,5 @@ if __name__ == "__main__":
         results[0][0], results[0][2], marker="X", edgecolors="black", linewidths=1
     )
     plt.show()
-    pickle.dump(fig, open("poincareplot.pkl", "wb"))
+
+    pickle.dump(fig, open("poincareplot_tokh.pkl", "wb"))
