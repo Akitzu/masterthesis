5235150425c48cecae963bc50309b6e58ba448c9
Akitzu <ludovic.rais@epfl.ch>
2024-05-22 20:59:22 +0200

    save #118

---
+++ runs/toybox-tol-0605/fig_multiplescan.py
@@ -0,0 +1,40 @@
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('lateky')
+
+# Load the data
+flist = ['perturbed-6-1/amplitude_scan_2/results.pkl',
+         'perturbed-12-2/amplitude_scan/results.pkl', 
+         'perturbed-18-3/amplitude_scan/results.pkl']
+
+for file in flist:
+    result = pickle.load(open(file, "rb"))
+
+    a = []
+    r = []
+    err_by_diff = []
+    err_by_estim = []
+    isok = []
+    for res in result:
+        if len(res) == 0:
+            continue
+        for ra in res:
+            a.append(ra[0])
+
+            rtmp = ra[1][:,0]
+            err_by_diff_tmp = ra[1][:,1]
+            err_by_estim_tmp = ra[1][:,2]
+            r.append(rtmp[rtmp > 0].sum())
+            err_by_diff.append(err_by_diff_tmp[rtmp > 0].sum())
+            err_by_estim.append(err_by_estim_tmp[rtmp > 0].sum())
+            isok.append(np.isclose(rtmp.sum(), 0, atol=1e-2*np.abs(rtmp).max()))
+    
+    isok = np.array(isok, dtype=bool)
+    a = np.array(a)
+    r = np.array(r)
+    err_by_diff = np.array(err_by_diff)
+    # Plot the data
+    plt.errorbar(a[isok], r[isok], err_by_diff[isok], fmt='s', markersize=1)
+
+plt.show()
--- runs/toybox-tol-0605/perturbed-12-2/amplitude_scan/fig.py
+++ runs/toybox-tol-0605/perturbed-12-2/amplitude_scan/fig.py
@@ -7,13 +7,21 @@ result = pickle.load(open("results.pkl", "rb"))
 
 a = []
 r = []
+err_by_diff = []
+err_by_estim = []
 for res in result:
     if len(res) == 0:
         continue
     for ra in res:
         a.append(ra[0])
-        r.append(ra[1][ra[1] > 0].sum())
+
+        rtmp = ra[1][:,0]
+        err_by_diff_tmp = ra[1][:,1]
+        err_by_estim_tmp = ra[1][:,2]
+        r.append(rtmp[rtmp > 0].sum())
+        err_by_diff.append(err_by_diff_tmp[rtmp > 0].sum())
+        err_by_estim.append(err_by_estim_tmp[rtmp > 0].sum())
 
 # Plot the data
-plt.scatter(a, r)
+plt.errorbar(a, r, err_by_diff, fmt='s', markersize=1)
 plt.show()
